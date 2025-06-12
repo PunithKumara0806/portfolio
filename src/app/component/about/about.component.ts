250250a7d8cf360a21911b91bedcb2d3448502f2
PunithKumara0806 <punithakumara623@outlook.com>
2025-06-12 11:29:25 +0530

    added about content

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { gsap } from 'gsap';
+
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+
+gsap.registerPlugin(DrawSVGPlugin, MotionPathPlugin);
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  onMouseEnter(): void {
+    gsap.fromTo(
+      '.cls-1',
+      {
+        drawSVG: '0%',
+      },
+      {
+        drawSVG: `0% 100%`,
+        duration: 2,
+      }
+    );
+  }
 }
